7c0d7484136c60d3358e8e4dd12dddfb6cea55ff
Sanghita Seal <sealsanghita2005@gmail.com>
2025-08-02 10:33:55 +0530

    set up - wrote the basic frontend of login & signup

--- my-app/src/app/login/page.tsx
+++ my-app/src/app/login/page.tsx
@@ -1,3 +1,49 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/navigation";
+import {axios} from "axios";
+
 export default function LoginPage(){
+    const [user, setUser] = React.useState({
+        email: "",
+        password: "",
+    });
+
+    const onLogin = async ()=>{
+
+    }
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">         
+            <h1>Login</h1>
+            <hr />
+            
+            <label htmlFor="email">email</label>
+            <input
+            className="p-2 border border-gray-300 rounded-lg mb-4 focus-outline-none focus:border-3 "
+                type="text"
+                id="email"
+                value={user.email}
+                onChange={(e) => setUser({...user, email: e.target.value})}  
+                placeholder="email"
+            />
+            <label htmlFor="password">password</label>
+            <input
+            className="p-2 border border-gray-300 rounded-lg mb-4 focus-outline-none focus:border-3  "
+                type="text"
+                id="password"
+                value={user.password}
+                onChange={(e) => setUser({...user, password: e.target.value})}  
+                placeholder="password"
+            />
+            <button 
+            onClick={onLogin}
+            className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-3">Login
+            </button>
+            <Link href="/signup" className="text-blue-500 hover:underline">
+                Don't have an account? Signup
+            </Link>
+        </div>
+    )
     
 }
